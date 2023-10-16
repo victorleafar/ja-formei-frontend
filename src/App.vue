2e18e686ae70078fc13465117fad5b3e12523764
Victor <victormartinezrc@gmail.com>
2023-10-16 11:31:29 -0300

    New Repository

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Logo da sua rede" src="./assets/logo.png">
+    <h1>Bem-vindo à nossa Rede para Ex-alunos!</h1>
+    <p>Atualmente, nossa plataforma está em construção.</p>
+    <p>Estamos trabalhando para criar um espaço onde ex-alunos possam se reconectar</p>
+    <p>compartilhar experiências acadêmicas e profissionais, e celebrar conquistas mútuas.</p>
+    <p>Fique atento para mais atualizações em breve!</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -23,4 +24,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#app img {
+  max-width: 500px;
+  margin-bottom: 20px;
+}
+
+#app h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+#app p {
+  font-size: 1.2em;
+  line-height: 1.5;
+  margin-bottom: 15px;
+}
 </style>
+
