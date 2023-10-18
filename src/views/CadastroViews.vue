45d3caab0c8bb1f736de3653fcd859ae1dbb55f8
Victor <victormartinezrc@gmail.com>
2023-10-18 09:12:24 -0300

    New Repository

---
+++ src/views/CadastroViews.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Tela de Cadastro</h1>
+      <!-- Seu formulário de cadastro aqui -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Lógica do componente de cadastro aqui
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para a tela de cadastro */
+  </style>
+  
