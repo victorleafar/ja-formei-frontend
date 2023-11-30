131ba3ecc14b89dc42b41f2f09392a2827f00a3e
Victor <victormartinezrc@gmail.com>
2023-11-30 10:24:54 -0300

    Update 10:24

--- src/views/HomeViews.vue
+++ src/views/HomeViews.vue
@@ -140,18 +140,35 @@
     <div id="right-sidebar">
       <!-- Eventos Próximos -->
       <div class="event-section">
-        <h2>Eventos Próximos</h2>
-        <ul>
-          <li><svg id="calendar" style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-              fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
-              <path
-                d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
-                fill="blue"></path>
-            </svg> Evento Próximo 1</li>
-          <li>Evento Próximo 2</li>
-          <li>Evento Próximo 3</li>
-        </ul>
-      </div>
+    <h2>Eventos Próximos</h2>
+    <ul>
+        <li>
+            <svg id="calendar" style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
+                    fill="blue"></path>
+            </svg> Evento Próximo 1
+        </li>
+        <li>
+            <svg id="calendar" style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
+                    fill="blue"></path>
+            </svg> Evento Próximo 2
+        </li>
+        <li>
+            <svg id="calendar" style="color: blue" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+                <path
+                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
+                    fill="blue"></path>
+            </svg> Evento Próximo 3
+        </li>
+    </ul>
+</div>
+
 
       <!-- Lista de Eventos -->
       <div class="event-section">
